94f057d7d6cb62938dcdbaef7eaaa5dc2da5614e
Nikita Krenov <nikitabaram@gmail.com>
2024-06-27 22:56:44 +0300

    first bib but it`s test varios

--- main.cpp
+++ main.cpp
@@ -3,8 +3,32 @@
 
 using namespace std;
 
+int string_length(const char* str);
+
 int main() {
+    int choice;
+	char str[] = "Hello, World!";
+	
+	cout << "Enter your choice: ";
+	cin >> choice;
+
+    switch (choice) {
+    case 1:
+        printf("String length: %d\n", string_length(str));
+        break;
+    default:
+        printf("Invalid choice!\n");
+        break;
+    }
 
 	system("pause");
 	return 0;
+}
+
+int string_length(const char* str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
 }
